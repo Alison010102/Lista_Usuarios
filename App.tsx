acc72ac795f4a372bf3d2f7ee3562f5b059f6116
Alison010102 <secundo2002@gmail.com>
2025-09-09 23:00:44 -0300

    modificando a parte inicial

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { View,Text } from "react-native"
 export default function App(){
   return(
     <View style={{flex:1,justifyContent:"center"}}>
-      <Text>Hello World</Text>
+      <Text>Iniciando a lista de usuarios</Text>
     </View>
 
   )
